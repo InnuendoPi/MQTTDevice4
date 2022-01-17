deb06ab26e078903146a048f20d8f377e961d6fa
InnuendoPi <k.ader@pc-team.de>
2022-01-17 13:34:50 +0100

    git
    Signed-off-by: InnuendoPi <k.ader@pc-team.de>

--- tools/buildFirm.cmd
+++
@@ -1,5 +0,0 @@
-copy build\MQTTDevice4.ino.bin tools\* /Y
-copy build\MQTTDevice4.mklittlefs.bin tools\* /Y
-cd tools
-del firmware.zip
-"C:\Program Files\7-Zip\7z.exe" a Firmware.zip MQTTDevice4.ino.bin MQTTDevice4.mklittlefs.bin Flashen.cmd esptool.exe
